fb101444b4f4fffd0e8ffa998ea2a1821a873c66
Dominik Russo <DominikRusso@tutanota.com>
2021-10-30 10:14:09 +0200

    Add login function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 mod args;
 
 use args::Subcommand;
+use huelib::bridge::Bridge;
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
+use std::net::IpAddr;
 use structopt::StructOpt;
 use xdg::BaseDirectories;
+
+static ENV_IP: &str = "HUE_IP";
+static ENV_USER: &str = "HUE_USER";
 
 fn main() {
     let subcommand = Subcommand::from_args();
@@ -14,7 +19,9 @@ fn main() {
         Subcommand::Init { username } => {
             let _ = init(&username);
         }
-        Subcommand::On { lights } => {}
+        Subcommand::On { lights } => {
+            let bridge = login();
+        }
         Subcommand::Off { lights } => {}
         Subcommand::Brightness { brightness, lights } => {}
         Subcommand::Color { color, lights } => {}
@@ -23,7 +30,6 @@ fn main() {
 }
 
 fn init(username: &str) -> Result<(), huelib::Error> {
-    use std::net::IpAddr;
     // damn this library's api is ugly
     use huelib::response::Error as ResponseError;
     use huelib::response::ErrorKind::LinkButtonNotPressed;
@@ -34,7 +40,7 @@ fn init(username: &str) -> Result<(), huelib::Error> {
     // check first if the user specified the IP and only then use
     // `discover_nupnp` which connects to Philip's severs
     // NOTE: I wonder if something like avahi could be used instead
-    let ip = match env::var("HUE_IP") {
+    let ip = match env::var(ENV_IP) {
         Ok(ip) => ip.parse::<IpAddr>().unwrap(),
         Err(_) => huelib::bridge::discover_nupnp()?
             .pop()
@@ -76,4 +82,32 @@ fn init(username: &str) -> Result<(), huelib::Error> {
     Ok(())
 }
 
-// fn check_initialized() -> bool {}
+fn login() -> Bridge {
+    let xdg_dirs = BaseDirectories::with_prefix("hue").unwrap();
+    let ip = match env::var(ENV_IP) {
+        Ok(ip) => ip
+            .parse::<IpAddr>()
+            .expect("Failed to parse IP address on `HUE_IP` environment variable."),
+        Err(_) => {
+            let bridge_path = xdg_dirs
+                .find_data_file("bridge")
+                .expect("Cannot find IP address of bridge in environment variable `HUE_IP` and the bridge file does not exist.");
+            fs::read_to_string(bridge_path)
+                .unwrap()
+                .parse()
+                .expect("Failed to parse IP address in username data file.")
+        }
+    };
+
+    let username = match env::var(ENV_USER) {
+        Ok(user) => user,
+        Err(_) => {
+            let username_path = xdg_dirs
+                .find_data_file("username")
+                .expect("Cannot find username in environment variable `HUE_USER` and the username file does not exist.");
+            fs::read_to_string(username_path).unwrap()
+        }
+    };
+
+    Bridge::new(ip, username)
+}
