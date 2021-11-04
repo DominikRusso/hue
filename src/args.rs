72a668214786b6194b74a903f5cfc2e2e75737e6
Dominik Russo <DominikRusso@tutanota.com>
2021-11-04 22:08:57 +0100

    Add help message documentation

--- src/args.rs
+++ src/args.rs
@@ -3,27 +3,40 @@ use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 pub enum Subcommand {
-    Init {
-        username: String,
-    },
-    On {
-        lights: Vec<String>,
-    },
-    Off {
-        lights: Vec<String>,
-    },
+    /// For pairing with the hue bridge on the network
+    Init { username: String },
+
+    /// For turning lights on
+    ///
+    /// Turns the specified lights on. If no lights are specified all lights
+    /// are turned on.
+    On { lights: Vec<String> },
+
+    /// For turning lights off
+    ///
+    /// Turns the specified lights off. If no lights are specified all lights
+    /// are turned off.
+    Off { lights: Vec<String> },
+
+    /// For controlling lights' brightness
+    ///
+    /// Sets the brightness of the specified lights. If no lights are specified
+    /// it sets the brightness of all lights.
     #[structopt(alias = "bri", settings = &[AppSettings::AllowNegativeNumbers])]
     Brightness {
         brightness: String,
         lights: Vec<String>,
     },
+
+    // TODO
+    /// For controlling lights' color (unimplemented)
+    ///
+    /// Sets the color of the specified lights. If no lights are specified it
+    /// sets the color of all lights.
     #[structopt(alias = "col")]
-    Color {
-        color: String,
-        lights: Vec<String>,
-    },
+    Color { color: String, lights: Vec<String> },
+
+    /// For setting scenes
     #[structopt(alias = "set")]
-    Scene {
-        name: String,
-    },
+    Scene { name: String },
 }
